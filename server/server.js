3e103756f95fcab11fd7f90d6d6b8955257fa8f9
Dylan Swoope <swoopedj@gmail.com>
2016-06-23 12:17:51 -0500

    initialize server, basic index.html, some dependencies

---
+++ server/server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var Path = require('path');
+var morgan = require('morgan');
+require('es6-promise').polyfill();
+var routes = express.Router();
+
+
+var data_api = require('./api/data-api.js')
+
+routes.use(morgan('dev'));
+
+
+//route to index.html
+var assetFolder = Path.resolve(__dirname, '../client/');
+routes.use(express.static(assetFolder));
+var angular = Path.resolve(__dirname, '../node_modules/')
+routes.use('/scripts', express.static(angular));
+
+//api routes
+routes.use('/api', results_api);
+
+if(process.env.NODE_ENV !== 'test'){
+  routes.get('/*', function(request, response){
+    response.sendFile(assetFolder + '/index.html');
+  });
+
+  //create server
+  var app = express();
+  app.use(require('body-parser').json())
+
+  //main router
+  app.use('/', routes)
+
+  //start server
+  var port = process.env.PORT || 8080;
+  app.listen(port, function(){
+    console.log('listening on port '+ port);
+  });
+} else {
+  //for test env
+  module.exports = routes;
+}

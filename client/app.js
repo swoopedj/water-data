3be69151ec32c024f1d81e71815479b0bf4e5bd6
Dylan Swoope <swoopedj@gmail.com>
2016-06-30 15:26:40 -0500

    hitting usgs endpoint with site ID and hitting google maps api with address to get lat long

--- client/app.js
+++ client/app.js
@@ -4,33 +4,12 @@ require('es6-promise').polyfill();
 
 var Api = module.exports;
 
-
-
 Api.getDataBySiteId = function(){
-console.log('Function Called!');
-// console.log('Id: ', id)
-var siteId = document.getElementById('site-id').value;
-// console.log('SiteId: ', siteId) // good
-var base_Url = 'http://waterservices.usgs.gov/nwis/iv/?&format=json,1.1&parameterCd=00065,00060&sites=' + siteId
-// console.log(baseUrl) // good
+  console.log('Function Called!');
+  var siteId = document.getElementById('site-id').value;
+  var baseUrl = 'http://waterservices.usgs.gov/nwis/iv/?&format=json,1.1&parameterCd=00065,00060&sites=' + siteId
 
-    // fetch(base_Url)
-    //     .then(function(response){
-    //       console.log('inside first then')
-    //       if(response.status >= 400){
-    //         throw new Error('Bad response from server');
-    //       }
-    //       return response.json();
-    //     })
-    //     .then(function(data){
-    //       console.log('Gage Height: ', data.value.timeSeries[1].values[0].value[0].value);
-    //       console.log('Discharge: ', data.value.timeSeries[0].values[0].value[0].value);
-    //       console.log('Discharge Time: ', data.value.timeSeries[0].values[0].value[0].dateTime)
-    //     });
-
-
-
-  request.get(base_Url, function (error, response, body) {
+  request.get(baseUrl, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var data = JSON.parse(body)
         console.log('Body: ', data) // Show the HTML for the Google homepage. 
@@ -41,13 +20,57 @@ var base_Url = 'http://waterservices.usgs.gov/nwis/iv/?&format=json,1.1&paramete
       }
   })
     
+}
 
+Api.getDataByCoordinates = function(coordinates, radius){
+  var baseUrl = 'http://waterservices.usgs.gov/nwis/iv/?&format=json,1.1&parameterCd=00065,00060&bBox=';
+  console.log('In getDataByCoordinates, coordinates: ', coordinates, ' radius: ', radius);
 
 
 }
 
-var button = document.getElementById('submit-button');
-button.addEventListener('click', Api.getDataBySiteId)
+
+
+Api.getCoordinatesByAddress = function(){
+  var baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+  var googleApiToken = require('./lib/apitoken.js');
+  var exampleUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + googleApiToken;
+  var address = {
+    street: document.getElementById("address-street").value || '',
+    city: document.getElementById("address-city").value,
+    state: document.getElementById("address-state").value
+  }
+  if(!address.city || !address.state){
+    var error = "Please provide a city and state."
+    console.log(error);
+    throw new Error(error);
+  }
+
+  var radius = Number(document.getElementById('location-radius').value) || 1;
+
+  console.log('Radius: ', radius)
+
+  var realUrl = baseUrl + address.street + ',' + address.city + ',' + address.state +'&key=' + googleApiToken;
+  console.log('realUrl: ', realUrl)
+
+  request.get(exampleUrl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body)
+        console.log('Body: ', data.results[0].geometry.location) // Show the HTML for the Google homepage. 
+        return data.results[0].geometry.location
+      }
+  }).then(function(coordinates){
+    console.log('Inside of THEN')
+    Api.getDataByCoordinates(coordinates, radius);
+  })
+
+}
+
+var usgsButton = document.getElementById('usgs-button');
+usgsButton.addEventListener('click', Api.getDataBySiteId)
+
+var googleButton = document.getElementById('google-button');
+googleButton.addEventListener('click', Api.getCoordinatesByAddress)
 
 
   //WORKS: (out here)

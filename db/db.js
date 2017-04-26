e7b5e2f610c1e40a672543f5a6bff468eebe5629
Dylan Swoope <dylanswoope@Dylans-MacBook-Pro.local>
2017-04-26 10:32:30 -0500

    using env variables for db connection

--- db/db.js
+++ db/db.js
@@ -1,12 +1,11 @@
 var pgp = require('pg-promise')();
 var pg_config = {
-  host: 'localhost', // Server hosting the postgres database 
-  port: 5432, //env var: PGPORT 
-  database: 'waterdata', //env var: PGDATABASE
-  user: 'dylanswoope', //env var: PGUSER 
-  password: 'bnm88990', //env var: PGPASSWORD 
-
-  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed 
+  host: 'localhost',  
+  port: 5432, 
+  database: 'waterdata', 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASS,  
+  idleTimeoutMillis: 30000 
 };
 var db = pgp(pg_config);
 
